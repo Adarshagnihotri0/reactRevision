e05af7e7e9eff8c94c8f7d087cc4054e794ac9a3
Adarsh Agnihotri <adarshagnihotri@Adarshs-MacBook-Air.local>
2026-03-23 15:08:10 +0530

    card import

---
+++ src/01 - components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div
+        style={{
+          border: "3px solid #4d0303",
+          borderRadius: "10px",
+          background: "#cb5959",
+          width: "330px",
+          padding: "30px",
+        }}
+      >
+        <h2>Card Title</h2>
+        <p>Card description goes here.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
